cdc672e0c757c8cef994d0b9b15890c56a5f9425
BitongJam <jamesortizalter@gmail.com>
2024-12-04 14:10:52 +0800

    [FIX] remove discount fix fucntionality it can cause conflict on the past transaction with percentage discount

--- pos_customs/static/src/pos/models.js
+++ pos_customs/static/src/pos/models.js
@@ -17,46 +17,46 @@ odoo.define('pos_customs.pos_extension', function (require) {
                 return result;
             }
 
-            // overrided
-            get_all_prices(qty = this.get_quantity()){
-                var price_unit = this.get_unit_price()
-                var taxtotal = 0;
+            // // overrided
+            // get_all_prices(qty = this.get_quantity()){
+            //     var price_unit = this.get_unit_price()
+            //     var taxtotal = 0;
         
-                var product =  this.get_product();
-                var taxes_ids = this.tax_ids || product.taxes_id;
-                taxes_ids = _.filter(taxes_ids, t => t in this.pos.taxes_by_id);
-                var taxdetail = {};
-                var product_taxes = this.pos.get_taxes_after_fp(taxes_ids, this.order.fiscal_position);
+            //     var product =  this.get_product();
+            //     var taxes_ids = this.tax_ids || product.taxes_id;
+            //     taxes_ids = _.filter(taxes_ids, t => t in this.pos.taxes_by_id);
+            //     var taxdetail = {};
+            //     var product_taxes = this.pos.get_taxes_after_fp(taxes_ids, this.order.fiscal_position);
         
-                var all_taxes = this.compute_all(product_taxes, price_unit, qty, this.pos.currency.rounding);
-                var all_taxes_before_discount = this.compute_all(product_taxes, price_unit, qty, this.pos.currency.rounding);
-                _(all_taxes.taxes).each(function(tax) {
-                    taxtotal += tax.amount;
-                    taxdetail[tax.id] = {
-                        amount: tax.amount,
-                        base: tax.base,
-                    };
-                });
+            //     var all_taxes = this.compute_all(product_taxes, price_unit, qty, this.pos.currency.rounding);
+            //     var all_taxes_before_discount = this.compute_all(product_taxes, price_unit, qty, this.pos.currency.rounding);
+            //     _(all_taxes.taxes).each(function(tax) {
+            //         taxtotal += tax.amount;
+            //         taxdetail[tax.id] = {
+            //             amount: tax.amount,
+            //             base: tax.base,
+            //         };
+            //     });
         
-                return {
-                    "priceWithTax": all_taxes.total_included - this.get_discount(),
-                    "priceWithoutTax": all_taxes.total_excluded - this.get_discount(),
-                    "priceWithTaxBeforeDiscount": all_taxes_before_discount.total_included,
-                    "priceWithoutTaxBeforeDiscount": all_taxes_before_discount.total_excluded,
-                    "tax": taxtotal - this.get_discount(),
-                    "taxDetails": taxdetail,
-                    "tax_percentages": product_taxes.map((tax) => tax.amount),
-                };
-            }
+            //     return {
+            //         "priceWithTax": all_taxes.total_included - this.get_discount(),
+            //         "priceWithoutTax": all_taxes.total_excluded - this.get_discount(),
+            //         "priceWithTaxBeforeDiscount": all_taxes_before_discount.total_included,
+            //         "priceWithoutTaxBeforeDiscount": all_taxes_before_discount.total_excluded,
+            //         "tax": taxtotal,
+            //         "taxDetails": taxdetail,
+            //         "tax_percentages": product_taxes.map((tax) => tax.amount),
+            //     };
+            // }
 
-            // overrided
-            // remove the constraint on minimum 0 to max 100
-            set_discount(discount){
-                // var parsed_discount = typeof(discount) === 'number' ? discount : isNaN(parseFloat(discount)) ? 0 : field_utils.parse.float('' + discount);
-                var disc = discount;
-                this.discount = disc;
-                this.discountStr = '' + disc;
-            }
+            // // overrided
+            // // remove the constraint on minimum 0 to max 100
+            // set_discount(discount){
+            //     // var parsed_discount = typeof(discount) === 'number' ? discount : isNaN(parseFloat(discount)) ? 0 : field_utils.parse.float('' + discount);
+            //     var disc = discount;
+            //     this.discount = disc;
+            //     this.discountStr = '' + disc;
+            // }
         };
 
     const PosOrderCustoms = (Order) => class PosOrderCustoms extends Order{
