af68aef2fc352d4d20678f1cbf041b69c4217c33
BitongJam <jamesortizalter@gmail.com>
2024-12-08 12:08:50 +0800

    on dev

---
+++ jo_pos_fix_discount/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'PoS Fixed Discount',
+    'version': '16.0',
+    'description': '''
+        PoS Fixed Discount
+         - fixed Discount per Order Line
+    ''',
+    'summary': 'Fixed Discount in PoS Lines',
+    'author': 'James Michael Ortiz',
+    'website': '',
+    'category': 'Sale',
+    'depends': [
+        'point_of_sale'
+    ],
+    'data': [
+        ''
+    ],
+    'auto_install': False,
+    'application': False,
+    'assets': {
+        
+    }
+}
