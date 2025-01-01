79240b5f1bbbd6dbdf9bd444d3028cb1f9440015
BitongJam <jamesortizalter@gmail.com>
2025-01-01 12:27:31 +0800

    [FIX] print session report when clossing session

--- pos_customs/static/src/pos/ClosePosPopup/ClosePosPopup.js
+++ pos_customs/static/src/pos/ClosePosPopup/ClosePosPopup.js
@@ -34,6 +34,66 @@ const ClosePosPopupExtend = (ClosePosPopup) =>
                 this.defaultCashDetails['starting'] = 'Error fetching session data';
             }
         }
+        // overrided
+        async closeSession() {
+            if (!this.closeSessionClicked) {
+                this.closeSessionClicked = true;
+                let response;
+                // If there are orders in the db left unsynced, we try to sync.
+                await this.env.pos.push_orders_with_closing_popup();
+                if (this.cashControl) {
+                     response = await this.rpc({
+                        model: 'pos.session',
+                        method: 'post_closing_cash_details',
+                        args: [this.env.pos.pos_session.id],
+                        kwargs: {
+                            counted_cash: this.state.payments[this.defaultCashDetails.id].counted,
+                        }
+                    })
+                    if (!response.successful) {
+                        return this.handleClosingError(response);
+                    }
+                }
+                await this.rpc({
+                    model: 'pos.session',
+                    method: 'update_closing_control_state_session',
+                    args: [this.env.pos.pos_session.id, this.state.notes]
+                })
+                try {
+                    const bankPaymentMethodDiffPairs = this.otherPaymentMethods
+                        .filter((pm) => pm.type == 'bank')
+                        .map((pm) => [pm.id, this.state.payments[pm.id].difference]);
+                    response = await this.rpc({
+                        model: 'pos.session',
+                        method: 'close_session_from_ui',
+                        args: [this.env.pos.pos_session.id, bankPaymentMethodDiffPairs],
+                        context: this.env.session.user_context,
+                    });
+                    if (!response.successful) {
+                        return this.handleClosingError(response);
+                    }
+                    await this.downloadSalesReport() //this is the added
+                    window.location = '/web#action=point_of_sale.action_client_pos_menu';
+                } catch (error) {
+                    const iError = identifyError(error);
+                    if (iError instanceof ConnectionLostError || iError instanceof ConnectionAbortedError) {
+                        await this.showPopup('ErrorPopup', {
+                            title: this.env._t('Network Error'),
+                            body: this.env._t('Cannot close the session when offline.'),
+                        });
+                    } else {
+                        await this.showPopup('ErrorPopup', {
+                            title: this.env._t('Closing session error'),
+                            body: this.env._t(
+                                'An error has occurred when trying to close the session.\n' +
+                                'You will be redirected to the back-end to manually close the session.')
+                        })
+                        window.location = '/web#action=point_of_sale.action_client_pos_menu';
+                    }
+                }
+                this.closeSessionClicked = false;
+            }
+        }
     };
 
 // Register the extended popup in the POS Registry
