776797c9bc012d807b5a6662552c8e2c9f3c5d6d
BitongJam <jamesortizalter@gmail.com>
2025-03-18 17:49:25 +0800

    [ADD] pos_sale_order_archving: wizard for batch pos order archive

---
+++ pos_sale_order_archiving/wizard/wizard_batch_pos_order_archive.py
@@ -0,0 +1,18 @@
+from odoo import _, api, fields, models
+
+
+
+class WizardBatchPosOrderArchive(models.AbstractModel):
+    _name = 'wizard.batch.pos.order.archive'
+    _description = 'Wizard Batch Pos Order Archive'
+
+    from_date = fields.Date(string='From')
+    to_date = fields.Date(string='To')
+    
+    def action_pos_order_archive(self):
+        from_date = self.from_date
+        to_date = self.to_date
+
+        data = self.env['pos.order'].get_order_to_archive_date_range(from_date,to_date)
+        raise UserWarning(data)
+        return
