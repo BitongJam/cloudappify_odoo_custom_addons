a6606f7a83a9e83304e4dc78b580892800e7d072
BitongJam <jamesortizalter@gmail.com>
2025-03-20 22:59:47 +0800

    [IMP] new update on pos_sale_order_archiving
    rename wiz_batch_pos_order_archive to wiz_manual_pos_order_archive

--- pos_sale_order_archiving/wizard/wizard_batch_pos_order_archive.py
+++ pos_sale_order_archiving/wizard/wizard_batch_pos_order_archive.py
@@ -5,65 +5,65 @@ from odoo.exceptions import ValidationError
 from dateutil.relativedelta import relativedelta
 from datetime import datetime
 
+date_selecetion = [
+    ('1', 'January'),
+    ('2', 'February'),
+    ('3', 'March'),
+    ('4', 'April'),
+    ('5', 'May'),
+    ('6', 'June'),
+    ('7', 'July'),
+    ('8', 'August'),
+    ('9', 'September'),
+    ('10', 'October'),
+    ('11', 'November'),
+    ('12', 'December')
+]
 
 class WizardBatchPosOrderArchive(models.TransientModel):
-    _name = 'wizard.batch.pos.order.archive'
+    _name = 'wizard.manual.pos.order.archive'
     _description = 'Wizard Batch Pos Order Archive'
 
-    from_date = fields.Date(string='From')
-    to_date = fields.Date(string='To')
+    month = fields.Selection(string='', selection=date_selecetion,default='1',required=True)
+    years = fields.Char(default=str(datetime.now().year),required=True)
+    
+    @api.onchange('years')
+    def _onchange_years(self):
+        if self.years:
+            if not self.years.isdigit():
+                raise ValidationError('Please enter a valid year.')
+    
     
     def action_pos_order_archive(self):
         """Triggers archiving per month based on date range."""
-        from_date = self.from_date
-        to_date = self.to_date
+        # from_date = self.from_date
+        # to_date = self.to_date
+        if not self.years:
+            raise ValidationError('Please enter a year.')
+        if not self.month:
+            raise ValidationError('Please select a month.')
         
         param_env = self.env['ir.config_parameter'].sudo()
 
         # Fetch min/max archive limits from config
         max_target = float(param_env.get_param('pos_order_limit.max_amount'))
         min_target = float(param_env.get_param('pos_order_limit.min_amount'))
+        month = int(self.month)
+        year = int(self.years)
 
-        self.env['pos.order'].get_orders_to_archive(min_target,max_target,from_date.month,from_date.year)
-        # # Start from the first day of the month
-        # current_date = from_date.replace(day=1)
-
-        # orders_to_archive = []  # Collect all orders across months
-
-        # while current_date <= to_date:
-        #     # Get the first and last day of the current month
-        #     first_date = current_date
-        #     last_day = calendar.monthrange(current_date.year, current_date.month)[1]
-        #     last_date = datetime(current_date.year, current_date.month, last_day).date()
-
-        #     # If last_date exceeds to_date, set it to to_date
-        #     if last_date > to_date:
-        #         last_date = to_date
-
-        #     # Call the function for this specific month
-        #     month_orders = self.env['pos.order'].get_orders_to_archive(
-        #         min_target, max_target, current_date.month, current_date.year
-        #     )
-
-        #     if month_orders:
-        #         orders_to_archive.extend(month_orders)
-
-        #     print(f"📅 Processing {current_date.strftime('%Y-%m')} ({first_date} to {last_date}) → Orders: {month_orders}")
-
-        #     # Move to the next month
-        #     current_date = current_date + relativedelta(months=1)
-
-        # if not orders_to_archive:
-        #     raise ValidationError('There are no orders to archive.')
+        get_order_ids = self.env['pos.order'].get_orders_to_archive(min_target,max_target,month,year)
 
         # # Archive orders
-        # orders = self.env['pos.order'].browse(orders_to_archive)
-        # for order in orders:
-        #     order.action_archive()
-        #     for p in order.payment_ids:
-        #         p.action_archive()
+        if not get_order_ids:
+            raise ValidationError('There is no orders')
+    
+        orders_to_archive = self.env['pos.order'].browse(get_order_ids)
+        for order in orders_to_archive:
+            order.action_archive()
+            for p in order.payment_ids:
+                p.action_archive()
 
-        # # if orders_to_archive:
-        # #     self._create_archiving_log(True, orders_to_archive)
+        if orders_to_archive:
+            self.env['pos.order']._create_archiving_log(True, orders_to_archive)
 
-        # return True
+        return True
