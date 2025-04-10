46923afe1b8cc94304bcf9dbe566c8079b2fbbd7
BitongJam <jamesortizalter@gmail.com>
2025-04-11 00:22:37 +0800

    first commit for pos_kitchen_receipt_order

---
+++ pos_kitchen_receipt_order/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'POS Kitchen Receipt Order',
+    'version': '16.0',
+    'description': '''
+        It will extend the PoS Order Receipt with the kitchen receipt Order.
+    ''',
+    'summary': 'It will extend the PoS Order Receipt with the kitchen receipt Order.',
+    'author': 'James Michael Ortiz',
+    'license': 'LGPL-3',
+    'category': 'Point of Sale',
+    'depends': [
+        'point_of_sale'
+    ],
+    'data':[
+        'views/pos_config_views.xml'
+    ],
+    'auto_install': False,
+    'application': False,
+}
