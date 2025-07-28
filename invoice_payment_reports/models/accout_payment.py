71ea4124be70d74dda7303d6fe1de3801e881b5c
BitongJam <jamesortizalter@gmail.com>
2025-07-28 15:28:35 +0800

    Update on  invoice_pyament_request [FIX] wrong field parameter call computed it must computer [IMP] include curency on amount_words

--- invoice_payment_reports/models/accout_payment.py
+++ invoice_payment_reports/models/accout_payment.py
@@ -6,14 +6,19 @@ from num2words import num2words
 class AccountPayment(models.Model):
     _inherit = 'account.payment'
 
-    amount_words = fields.Char(computed = "_compute_amount_words")
+    amount_words = fields.Char(compute = "_compute_amount_words")
 
-    @api.depends("amount")
+    @api.depends("amount","currency_id")
     def _compute_amount_words(self):
-        # This will compute to convert amount field to amount in words for check 
         for rec in self:
             if rec.amount:
-                rec.amount_words = num2words(rec.amount, lang='en')
+                # Convert amount to words
+                words = num2words(rec.amount, lang='en').capitalize()
+                # Append currency name
+                if rec.currency_id:
+                    words = f"{words} {rec.currency_id.name}"
+                rec.amount_words = words
             else:
                 rec.amount_words = ''
-    
+
+
