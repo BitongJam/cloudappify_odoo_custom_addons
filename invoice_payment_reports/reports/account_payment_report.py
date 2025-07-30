47e9b5e5e4ee31c3d53572f6bcc9a1fe7f5a6253
BitongJam <jamesortizalter@gmail.com>
2025-07-30 23:31:33 +0800

    Update on Reports - Update AP Voucher - Update Check voucher - New Cash Voucher

--- invoice_payment_reports/reports/account_payment_report.py
+++ invoice_payment_reports/reports/account_payment_report.py
@@ -1,4 +1,5 @@
 from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError, UserError
 
 
 
@@ -10,9 +11,39 @@ class CheckVoucherReport(models.AbstractModel):
     def _get_report_values(self, docids, data=None):
         docs = self.env['account.payment'].browse(docids)     
 
-        total_debit = 0;
-        total_credit = 0;
+        total_debit = 0
+        total_credit = 0
         for rec in docs:
+            if rec.journal_id.type != 'bank':
+                raise UserError(_('This report is only for bank payments.'))
+            
+            for line in rec.move_id.line_ids:
+                total_debit = total_debit + line.debit
+                total_credit = total_credit + line.credit
+
+        payment_dict = {'total_debit': total_debit, 'total_credit': total_credit}
+
+    
+        return {
+            'doc_ids': docids,
+            'doc_model': 'account.payment',
+            'docs': docs,
+            'payment_dict': payment_dict,
+        }
+    
+class CashVoucerhReport(models.AbstractModel):
+    _name = 'report.invoice_payment_reports.cash_voucher'
+    _description = 'Cash Voucher Report'
+
+    def _get_report_values(self, docids, data=None):
+        docs = self.env['account.payment'].browse(docids)     
+
+        total_debit = 0
+        total_credit = 0
+        for rec in docs:
+            if rec.journal_id.type != 'cash':
+                raise UserError(_('This report is only for cash payments.'))
+            
             for line in rec.move_id.line_ids:
                 total_debit = total_debit + line.debit
                 total_credit = total_credit + line.credit
