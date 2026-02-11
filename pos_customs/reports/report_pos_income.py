9a9d9d1ed9d374c2de75d0f64ecb657e8a007b6e
BitonJam <ubuntu-22@james.localdomain>
2026-02-11 22:23:14 +0800

    [FIX] pos income report filtiring date time not computed with timezone

--- pos_customs/reports/report_pos_income.py
+++ pos_customs/reports/report_pos_income.py
@@ -1,5 +1,8 @@
-from odoo import models, api
+from odoo import models, api,fields
 from collections import defaultdict
+from datetime import datetime, time
+import pytz
+
 
 class PosIncomeReport(models.AbstractModel):
     _name = 'report.pos_customs.pos_income_report_template'
@@ -11,11 +14,25 @@ class PosIncomeReport(models.AbstractModel):
         date_from = data.get('date_from')
         date_to = data.get('date_to')
 
-        # Fetch POS payments based on the date range
-        pos_payments = self.env['pos.payment'].search([
-            ('pos_order_id.date_order', '>=', date_from),
-            ('pos_order_id.date_order', '<=', date_to),
+        user_tz = pytz.timezone(self.env.context.get('tz') or self.env.user.tz or 'UTC')
+
+        # convert date_from → start of day in user tz → UTC
+        date_from_dt = user_tz.localize(
+            fields.Datetime.from_string(date_from + " 00:00:00")
+        ).astimezone(pytz.UTC).replace(tzinfo=None)
+
+        # convert date_to → end of day in user tz → UTC
+        date_to_dt = user_tz.localize(
+            fields.Datetime.from_string(date_to + " 23:59:59")
+        ).astimezone(pytz.UTC).replace(tzinfo=None)
+
+        orders = self.env['pos.order'].search([
+            ('date_order', '>=', fields.Datetime.to_string(date_from_dt)),
+            ('date_order', '<=', fields.Datetime.to_string(date_to_dt)),
+            ('state', 'in', ['paid', 'invoiced', 'done'])
         ])
+        # Fetch POS payments based on the date range
+        pos_payments = self.env['pos.payment'].search([('pos_order_id','in',orders.ids)])
 
         # Group income by payment method
         income_by_method = defaultdict(float)
